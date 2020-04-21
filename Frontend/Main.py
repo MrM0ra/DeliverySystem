33ebac537fddcacf05f8363c4cf203a77c47949f
unknown <themonstermora@gmail.com>
2020-04-21 18:04:03 -0500

    baby steps on sessions, best flashing messages, i'll add some DB stuff and user saving thing later on

--- Frontend/Main.py
+++ Frontend/Main.py
@@ -1,11 +1,10 @@
-from flask import Flask, redirect, url_for, render_template, request, flash
+from flask import Flask, redirect, url_for, render_template, request, flash, session
 
 class Main:
 
 	app=Flask(__name__)
 
 	app.secret_key = "thisissupposedtobesecret"
-
 
 	@app.route("/")
 	def home():
@@ -16,12 +15,18 @@ class Main:
 	def login():
 		if request.method == "POST":
 			if request.form["IDF"] == "admin" and request.form["pwd"] == "admin":
-				return redirect(url_for("dashboard"))
+				user=request.form["IDF"]
+				session["user"]=user
+				return redirect(url_for("dashboard", user=user))
 			else:
-				flash("Usuario y/o contraseña invalida")
+				flash("Usuario y/o contraseña invalida", "warning")
 				return render_template("login.html")
 		else:
-			return render_template("login.html")
+			if "user" in session:
+				userName=session.get("user", None)
+				return redirect(url_for("dashboard", user=userName))
+
+		return render_template("login.html")
 
 
 	@app.route("/signup/")
@@ -29,24 +34,36 @@ class Main:
 		return render_template("signUp.html")
 
 
+	@app.route("/logout/")
+	def logout():
+		session.pop("user", None)
+		flash("Salida exitosa")
+		return redirect(url_for("login"))
+
+
 	@app.route("/dashboard/", methods=["POST", "GET"])
 	def dashboard():
-		if request.method == "POST":
-			if request.form["create_request"]:
-				return redirect(url_for("order_request"))
-			elif request.form["watch_request"] and request.method == "POST":
-				return redirect(url_for("order_request"))
-			elif request.form["watch_past_request"] and request.method == "POST":
-				return redirect(url_for("order_request"))
+		if "user" in session:
+			userName=session.get("user", None)
+			if request.method == "POST":
+				if request.form["create_request"]:
+					return redirect(url_for("order_request"))
+				elif request.form["watch_request"] and request.method == "POST":
+					return redirect(url_for("order_request"))
+				elif request.form["watch_past_request"] and request.method == "POST":
+					return redirect(url_for("order_request"))
+			else:
+				return render_template("userInterface.html", user=userName)
 		else:
-			return render_template("userInterface.html")
+			flash("Debe iniciar sesion para acceder a la pagina", "warning")
+			return redirect(url_for("login"))
 
 
 	@app.route("/order_request/", methods=["POST", "GET"])
 	def order_request():
 		if request.method=="POST":
 			return redirect(url_for("dashboard"))
-			flash("Orden efectuada correctamente")
+			flash("Orden efectuada correctamente", "success")
 		else:
 			return render_template("orderRequest.html")
 
