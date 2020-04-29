18a661a857f105c8d5ce06779075a407b0212e6a
Dilan Bustamante <dilanbustmart@gmail.com>
2020-04-29 00:31:55 -0500

    R5 Know assigned orders

--- Model/Enterprise.py
+++ Model/Enterprise.py
@@ -6,6 +6,7 @@ class Enterprise:
 
     users=[]
     stackOrder=[]
+    knowAorder =[]
 
     def __init__(self, name):
         self.name=name
@@ -38,6 +39,19 @@ class Enterprise:
         for i in Enterprise.users:
             if i.getName() == username:
                 return i
+
+
+    def knowAssignedOrders(self, order ):
+        if order.getState() == "Asignada" or order.getState() == "En camino" :
+            self.knowAorder.append(order)
+            messagebox.showinfo(message="Esta orden ya esta asignada", title="Atención")
+        if order.getState() =="No Asignada":
+            messagebox.showinfo(message="Esta orden no a sido asignada", title="Atención")
+        else:
+            messagebox.showinfo(message="Ha ocurrido un error, por favor intente de nuevo,", title="Atención")
+
+
+
 
     def modifyUser(self):
         return 0
