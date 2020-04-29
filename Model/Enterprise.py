26988d913166c86edf526d128b3cdaf3eb4d0b43
unknown <julianbr434@gmail.com>
2020-04-29 00:54:17 -0500

    join methods

--- Model/Enterprise.py
+++ Model/Enterprise.py
@@ -1,9 +1,13 @@
-from User import User
+from Model.Order import Order
+from Model.User import User
 import random
+from tkinter import messagebox
 
 class Enterprise:
 
     users=[]
+    stackOrder=[]
+    knowAorder =[]
 
     def __init__(self, name):
         self.name=name
@@ -17,6 +21,17 @@ class Enterprise:
     def setProfitType(self, profitType):
         return 0
 
+    def addOrder(self, idUser, senderLoc, destLoc, state, description):
+        theOrder = Order(senderLoc,destLoc,state,description)
+        Enterprise.stackOrder.append(theOrder)
+        i = 0
+        found = False
+        while(i<len(Enterprise.users) and (not found)):
+            if Enterprise.users[i].getId() == idUser:
+                found = True
+                Enterprise.users[i].getOrders().append(theOrder)
+            i = i+1
+        self.assignOrder(self,theOrder)
 
     def addUser(self, id, pwd, name, lastname, phone, idDoc):
         newUser=User(id, pwd, name, lastname, phone, idDoc)
@@ -26,6 +41,15 @@ class Enterprise:
         for i in Enterprise.users:
             if i.getName() == username:
                 return i
+
+    def knowAssignedOrders(self, order ):
+        if order.getState() == "Asignada" or order.getState() == "En camino" :
+            self.knowAorder.append(order)
+            messagebox.showinfo(message="Esta orden ya esta asignada", title="Atención")
+        if order.getState() =="No Asignada":
+            messagebox.showinfo(message="Esta orden no a sido asignada", title="Atención")
+        else:
+            messagebox.showinfo(message="Ha ocurrido un error, por favor intente de nuevo,", title="Atención")
 
     def modifyUser(self):
         return 0
@@ -41,6 +65,6 @@ class Enterprise:
         return couriers
 
     def assignOrder(self, order):
-        couriers = filterCourier(self)
+        couriers = self.filterCourier(self)
         random_courier = random.choices(couriers)
         random_courier.getOrders.appened(order)
